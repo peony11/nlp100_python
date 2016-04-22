a3e7968cfb4d852e1c1e990d0a77c13c5e3dd38e
peony11 <haruka.majima@gmail.com>
2016-04-22 12:06:35 +0900

    Update 02.py

--- 02.py
+++ 02.py
@@ -1,12 +1,10 @@
 """
 02. 「パトカー」＋「タクシー」＝「パタトクカシーー」
-
 「パトカー」＋「タクシー」の文字を先頭から交互に連結して文字列「パタトクカシーー」を得よ．
 """
-text1 = u"パトカー"
-text2 = u"タクシー"
+text_a = u"パトカー"
+text_b = u"タクシー"
 text = ''
-for i in range(4):
-    text += text1[i]
-    text += text2[i]
+for a, b in zip(text_a, text_b):
+    text += a + b
 print text
